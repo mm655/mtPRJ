98d5584215ddb31a66732f53dbabe498496ac2a2
ZhangMeng <kimizhang655@126.com>
2015-12-21 09:30:07 +0800

    new create pro

---
+++ mttt/mttt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mttt
+//
+//  Created by ZhangMeng on 15/12/21.
+//  Copyright © 2015年 ZhangMeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mttt/mttt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mttt
+//
+//  Created by ZhangMeng on 15/12/21.
+//  Copyright © 2015年 ZhangMeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
